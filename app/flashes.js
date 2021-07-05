0db11740ceed2fa162d1d168c775b4335b9e54ec
Arthur <a.dhuy@orange.fr>
2021-07-05 09:46:14 +0200

    update flashes

--- app/flashes.js
+++ app/flashes.js
@@ -10,9 +10,7 @@ const displayFlashes = () => {
 
   if(alert){
     setTimeout(function(){alert.style.right = '15px';}, 300);
-    alert.querySelector('.close').addEventListener('click', (event) => {
-      alert.style.right = '-400px';
-      setTimeout(function(){alert.remove();}, 1000);
-    });
+    setTimeout(function(){alert.style.right = '-400px';}, 6000);
+    setTimeout(function(){alert.remove();}, 7000);
   };
 };
